921ccf424f47a557abf2445b2a3078e25d375e2d
prarthanagalani <prarthanagalani@gmail.com>
2023-11-25 10:41:56 +0530

    added: Uc1: constructor to add contact-day5

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,8 +1,33 @@
-public class AddressBookSystem{
+class AddContacts {
 
-    public static void main(String[] args)
-    {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
+
+    // Uscase1: constructor to initialize contacts
+    public AddContacts(String firstName, String lastName, String address, String city, String state, String zip,
+            String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+}
+
+public class AddressBookSystem {
+
+    public static void main(String[] args) {
         System.out.println("Welcome to address book system program!!!");
-        
+
     }
 }
